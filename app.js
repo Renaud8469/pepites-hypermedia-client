34f198303184e4cfd65335270f59d48fd9850538
Renaud Dahl <renauddahl@gmail.com>
2017-07-05 15:29:59 +0200

    Little wording alteration to test continuous delivery

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express()
 app.set('port', (process.env.PORT || 5000));
 
 app.get('/', function (req, res) {
-	res.send('Hello World!')
+	res.send('Hello World! This is just the beginning.')
 })
 
 app.listen(app.get('port'), function () {
