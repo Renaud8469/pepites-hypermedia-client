d1928e13858eb91e676869ef238ab0dc721732d8
Renaud Dahl <renauddahl@gmail.com>
2017-07-12 14:38:30 +0200

    Successfully ping application route

--- app.js
+++ app.js
@@ -1,15 +1,26 @@
 const express = require('express')
 const app = express()
+const request = require('request')
 
 const config = require('./config')
 
 app.set('port', (process.env.PORT || 5000));
 
 app.get('/', function (req, res) {
-	res.send('Hello World! This is just the beginning.')
+	let application_ping, result = 'Hello World! <br> The API ';
+
+	request(config.api.url + 'api/application/ping', function(error, response, body) {
+		if (!error && response.statusCode == 200) {
+			application_ping = body;
+			result += 'responded well : ' + application_ping;
+		} else { 
+			result += 'did not respond well.';
+		}
+		res.send(result);
+	});
 })
 
 app.listen(app.get('port'), function () {
-	console.log('Example app listening on port 3000!')
+	console.log('Example app listening on port 5000!')
 })
 
