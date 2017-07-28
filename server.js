907619d03df193a6a8f574ac4df79eebdc11e6ef
daniel <dsabin@octo.com>
2017-07-28 14:34:52 +0200

    travaux sur les TU

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 const express = require('express')
 const app = express()
 
-const boot = require('./lib/boot')
+exports.app = app
 
-boot.setPort(app)
+exports.start = () => {
 
-boot.setRoutes(app)
 
-boot.startListening(app)
+
+}
+
+// const boot = require('./lib/boot')
+
+// boot.setPort(app)
+// // boot.setRoutes(app)
+// // boot.startListening(app)
